498492274eacaccb433cb23ea13de85da91c42bc
pkramadhati <kramadhati.prabha@gmail.com>
2022-02-23 00:31:05 -0800

    Version Something

--- Wiki/encyclopedia/urls.py
+++ Wiki/encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.page, name="page"),
+    path("search", views.search, name="search")
 ]
--- Wiki/encyclopedia/views.py
+++ Wiki/encyclopedia/views.py
@@ -1,6 +1,9 @@
+from encodings import search_function
+from pickle import GET
 from django.shortcuts import render
-
+from django import forms
 from . import util
+import markdown
 
 
 def index(request):
@@ -8,3 +11,29 @@ def index(request):
         "entries": util.list_entries()
     })
 
+# Entry Page #
+
+def page(request, title):
+
+  #util.get_entry(title) gets the content of the page from entries #
+  getContent=util.get_entry(title)
+   
+   #Check if the entry exits#
+  if getContent :
+        return render (request,"encyclopedia/page.html",{
+           "title":title,
+            #markdown.markdown converts markdown to html stores it in a variable called pageContent#
+           "pageContent":markdown.markdown(getContent)})
+    # Display error if it doesn't
+  else:
+        return render (request, "encyclopedia/error.html",{
+             "title":title
+        })
+#Search Page#
+
+def search(request):
+
+
+   if request.method == GET:
+        return render (request,"encyclopedia/search.html",{
+         })
